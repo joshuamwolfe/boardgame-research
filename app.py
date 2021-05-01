cd913437054b45ed93ab46f90dbd1e569f6e6b33
Joshua Wolfe <joshuamwolfe@gmail.com>
2021-05-01 12:20:22 -0400

    added html files, moved txt/pdf/md files, updated app.py

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
-from flask import Flask
+"""Flask app for dessert demo."""
+
+from flask import Flask, request, redirect, render_template, flash
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///boardgames"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+app.config["SECRET_KEY"] = "ihaveasecret"
+
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
+
+connect_db(app)
+db.create_all()
+
+
+def todict(future_dict):
+    """PLACHOLDER"""
+    return {"id": future_dict.id}
 
 
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def roote():
+    """PLACEHOLDER"""
+    return render_template("hom.html")
